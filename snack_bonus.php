d7cf06ffa21f19ffd61fa3e984d4c34bc6184a50
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-23 19:19:13 +0200

    Added full snack bonus and super bonus

--- snack_bonus.php
+++ snack_bonus.php
@@ -5,15 +5,32 @@
 - che age sia un numero.
 -Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SNACK 2</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+
+$user_data = [ $_GET["name"],  $_GET["email"], $_GET["age"] ];
+
+if (empty($user_data)) {
+    echo "Attenzione! non hai inserito alcun dato";
+} 
+//// Verify that all the entered fields are valid 
+else {
+    if(strlen($_GET["name"]) < 3 || !is_numeric($_GET["age"]) || !strpos($_GET["email"] , ".") || !strpos( $_GET["email"] , "@")) {
+        echo "<h1>Accesso negato</h1>";
+        //** If the entered name is not valid */
+        if(strlen($user_data[0]) < 3) {
+            echo "<p>Il nome inserito non è valido!</p>";
+        };
+        //** If the entered mail is not valid */
+        if( !strpos($_GET["email"] , ".") || !strpos( $_GET["email"] , "@") ) {
+            echo "<p>La mail non è valida!</p>";
+        }; 
+        //** If the entered age is not valid */
+        if(!is_numeric($user_data[2])) {
+            echo "<p>L'età inserita non è valida!</p>";       
+        };
+    } else {
+        echo "<h1>Accesso riuscito</h1>" ;       
+    }
+}
+?>
+
