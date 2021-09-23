00b99f03d392c3001a7fdb123b2bc2405b97c10c
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-23 17:12:28 +0200

    Snack2: Added array of students

--- snack_2.php
+++ snack_2.php
@@ -1,9 +1,36 @@
-<!-- /* ------------------------------- BONUS SNACK ------------------------------ */ -->
-<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
--che name sia più lungo di 3 caratteri,
--che mail contenga un punto e una chiocciola
-- che age sia un numero.
--Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+<!-- /* --------------------------------- SNACK 2 -------------------------------- */ -->
+<!-- Creare un array contenente qualche alunno di un’ipotetica classe.
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici 
+(senza, materia, solo numeri). 
+Creare una funzione che calcoli la media voto di un alunno.
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php 
+$students = [
+    [
+        "name" => "Sara",
+        "lastname" => "Bianchi", 
+        "grades" => [6, 8, 7, 7, 6, 9, 8, 6, 6]
+    ],
+    [
+        "name" => "Luca",
+        "lastname" => "Rossi", 
+        "grades" => [10, 9, 9, 9, 8, 10, 7, 8, 8]
+    ],
+    [
+        "name" => "Antonio",
+        "lastname" => "Verdi", 
+        "grades" => [6, 5, 5, 5, 4, 6, 6, 5, 6]
+    ],
+    [
+        "name" => "Maria",
+        "lastname" => "Ferrari", 
+        "grades" => [8, 8, 6, 7, 4, 8, 3, 7, 6]
+    ],
+];
+var_dump($students);
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,9 +38,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SNACK 2</title>
+    <title>SNACK BONUS</title>
 </head>
 <body>
     
 </body>
 </html>
+
--- snack_bonus.php
+++ snack_bonus.php
@@ -1,10 +1,9 @@
-<!-- /* --------------------------------- SNACK 2 -------------------------------- */ -->
-<!-- Creare un array contenente qualche alunno di un’ipotetica classe.
-Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici 
-(senza, materia, solo numeri). 
-Creare una funzione che calcoli la media voto di un alunno.
-Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
-
+<!-- /* ------------------------------- BONUS SNACK ------------------------------ */ -->
+<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
+-che name sia più lungo di 3 caratteri,
+-che mail contenga un punto e una chiocciola
+- che age sia un numero.
+-Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +11,7 @@ Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SNACK BONUS</title>
+    <title>SNACK 2</title>
 </head>
 <body>
     
